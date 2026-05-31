64512ee372d9007b058cfa32e6d0dcebbed9c99d
Jason Swift <anemele@outlook.com>
2026-05-31 11:58:07 +0800

    opt:linear sieve; new ui

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import array
-import math
 import time
 from pathlib import Path
+from typing import Any, Callable
 
-spf_list = [0, 1, 2, 3, 2, 5, 2]
-# Python 切片是浅拷贝，因此这里只保留奇素数
-prime_list = [
-    # 2,
-    3,
-    5,
-]
-N = 10000000
+N = 100000000
 PACK_FMT = "I"  # I: unsigned int
 
 db_path = Path("db")
@@ -20,56 +13,66 @@ fp_prime = db_path / "prime.txt"
 fp_bitmap = db_path / "bitmap.bin"
 
 
-def _op(num: int) -> None:
-    if not num & 1:
-        spf_list.append(2)
-        return
-
-    bound = math.isqrt(num)
-    for prime in prime_list:
-        if prime > bound:
-            break
-        if not num % prime:
-            spf_list.append(prime)
-            return
-
-    spf_list.append(num)
-    prime_list.append(num)
-
-
 def calc():
-    n = len(spf_list)
+    spf_list = [0] * N
+    prime_list = []
+
     start = time.perf_counter()
-    for num in range(n, N):
-        _op(num)
+    for i in range(2, N):
+        if spf_list[i] == 0:
+            spf_list[i] = i
+            prime_list.append(i)
+        for p in prime_list:
+            ip = i * p
+            if ip >= N:
+                break
+            spf_list[ip] = p
+            if p == spf_list[i]:
+                break
     end = time.perf_counter()
     print(f"calc {N} costs {end - start:.3f}s")
 
+    def dump():
+        with fp_spf.open("wb") as fp:
+            array.array(PACK_FMT, spf_list).tofile(fp)
 
-def dump():
-    with fp_spf.open("wb") as fp:
-        array.array(PACK_FMT, spf_list).tofile(fp)
+        with fp_prime.open("w") as fp:
+            fp.write("2\n")
+            fp.write("\n".join(map(str, prime_list)))
 
-    with fp_prime.open("w") as fp:
-        fp.write("2\n")
-        fp.write("\n".join(map(str, prime_list)))
+        # 移位运算优先级低于加减
+        bitmap = bytearray((N >> 3) + 1)
+        bitmap[0] |= 4  # 2
+        for prime in prime_list:
+            idx1 = prime >> 3
+            idx2 = prime & 7
+            bitmap[idx1] |= 1 << idx2
+        with fp_bitmap.open("wb") as fp:
+            fp.write(bitmap)
 
-    # 移位运算优先级低于加减
-    bitmap = bytearray((N >> 3) + 1)
-    bitmap[0] |= 4  # 2
-    for prime in prime_list:
-        idx1 = prime >> 3
-        idx2 = prime & 7
-        bitmap[idx1] |= 1 << idx2
-    with fp_bitmap.open("wb") as fp:
-        fp.write(bitmap)
+    return dump
 
 
-def check():
+def _ui(prompt: str, f: Callable[[int], Any]):
+    print(prompt)
+    while True:
+        s = input("\n? ").strip()
+        if not s:
+            break
+        elif s.isdigit():
+            n = int(s)
+            if n >= N:
+                print("over max db")
+            else:
+                print(f(n))
+        else:
+            print("invalid input")
+
+
+def expand():
     arr = array.array(PACK_FMT)
     arr.frombytes(fp_spf.read_bytes())
     spf_lst = arr.tolist()
-    bitmap = fp_bitmap.read_bytes()
 
     def expand_spf(n: int) -> str:
         parts = []
@@ -82,24 +85,17 @@ def check():
             parts.append(f"{p}^{cnt}" if cnt > 1 else str(p))
         return "*".join(parts)
 
-    def check(n: int) -> bool:
+    _ui("expand spf", expand_spf)
+
+
+def check():
+    bitmap = fp_bitmap.read_bytes()
+
+    def check_n(n: int) -> bool:
         idx1 = n >> 3
         idx2 = n & 7
         bit = bitmap[idx1] & (1 << idx2)
         is_prime = not not bit
-        if is_prime:
-            # print(n)
-            print("prime")
-        else:
-            print(expand_spf(n))
         return is_prime
 
-    print("test if an integer is prime")
-    while True:
-        s = input("\n? ").strip()
-        if not s:
-            break
-        elif s.isdigit():
-            check(int(s))
-        else:
-            print("invalid input")
+    _ui("test if an integer is prime", check_n)
